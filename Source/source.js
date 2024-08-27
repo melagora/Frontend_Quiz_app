b9fea29cdcdaac14fa3974cac60689457b8c00d8
Melvin González <54681730+melagora@users.noreply.github.com>
2024-08-27 13:12:13 -0600

    redireccion
    solo agregar en el source.js la ruta del html del tema que corresponde

--- Source/source.js
+++ Source/source.js
@@ -1,4 +1,20 @@
+function redirectToHTMLscriptQuiz() {
+  // Reemplaza 'https://tu-pagina-de-quiz-javascript.com' con la URL deseada
+  window.location.href = '';
+}
+
+function redirectToCSSscriptQuiz() {
+  // Reemplaza 'https://tu-pagina-de-quiz-javascript.com' con la URL deseada
+  window.location.href = '';
+}
+
 function redirectToJavascriptQuiz() {
     // Reemplaza 'https://tu-pagina-de-quiz-javascript.com' con la URL deseada
     window.location.href = '/Frontend_Quiz_app/Source/questionarios/cuiestionario-js/cuest_js.html';
   }
+
+  function redirectToBootStrapscriptQuiz() {
+    // Reemplaza 'https://tu-pagina-de-quiz-javascript.com' con la URL deseada
+    window.location.href = '';
+  }
+  
