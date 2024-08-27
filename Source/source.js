3de6436e87db3d4732970377bb38af6ecb0e7923
Melvin González <54681730+melagora@users.noreply.github.com>
2024-08-27 12:49:33 -0600

    redireccion a tema correspondiente
    Se agregó el js que contiene los metodos de redireccion a cada tema

---
+++ Source/source.js
@@ -0,0 +1,4 @@
+function redirectToJavascriptQuiz() {
+    // Reemplaza 'https://tu-pagina-de-quiz-javascript.com' con la URL deseada
+    window.location.href = '/Frontend_Quiz_app/Source/questionarios/cuiestionario-js/cuest_js.html';
+  }
