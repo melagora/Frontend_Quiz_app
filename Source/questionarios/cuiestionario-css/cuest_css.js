561acca65159257c825ca0a5d559eab5a504feea
Melvin González <54681730+melagora@users.noreply.github.com>
2024-08-28 23:27:07 -0600

    cambio orden preguntas
    Cambia el orden de como se muestran las preguntas al usuario al recargar la pagina

--- Source/questionarios/cuiestionario-css/cuest_css.js
+++ Source/questionarios/cuiestionario-css/cuest_css.js
@@ -25,6 +25,17 @@ let currentQuestionIndex = 0;
 let puntaje = 0; // Inicializar el puntaje
 const totalPreguntas = quizData.length;
 
+// Mezclar las preguntas aleatoriamente
+function cambiarOrdendelArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+cambiarOrdendelArray(quizData);
+
+
 // Función para cargar la pregunta actual
 function loadQuestion() {
 
